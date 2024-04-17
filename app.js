8d38ba78cc498f0551e310f249f899584e3bb42b
Angel solis <angel.solis.p@gmail.com>
2024-04-17 11:35:22 -0600

    main

--- app.js
+++ app.js
@@ -6,6 +6,27 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+//motor de plantilla
+app.set('view engine', 'ejs')
+
+//carpeta para archivos estaticos
+app.use(express.static('public'));
+
+//variables de entorno
+dotenv.config({ path: './.env/.env' });
+
+//trabajar con las cookies
+//app.use(cookieParser());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//llamando a las rutas
+app.use('/', require('./routes/router'));
+
+
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
