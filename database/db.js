09fb4c0c07316f49a9e659af54111b8d608a5636
Angel solis <angel.solis.p@gmail.com>
2024-04-19 22:52:20 -0600

    main

--- database/db.js
+++ database/db.js
@@ -1,22 +1,18 @@
 const mysql = require('mysql');
 
-const conexion = mysql.createConnection({	
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+const conexion = mysql.createConnection({
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_DATABASE,
+})
 
-});
-
-conexion.connect((error) => {
-
-    if (error) {
-        console.error('El error de conexión es: ' + error);
-        return;
+conexion.connect( (error)=> {
+    if(error){
+        console.log('El error de conexión es: '+error)
+        return
     }
+    console.log('¡Conectado a la base de datos MySQL!')
+})
 
-    console.log('Conectado a la base de datos MySQL');
-});
-
-module.exports = conexion;
-
+module.exports = conexion
